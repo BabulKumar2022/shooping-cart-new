34f5bf39f06f500b61b644bc5406905ace5d2249
BabulKumar2022 <bb3583400@gmail.com>
2022-10-18 01:09:43 +0600

    minuse add

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ document.getElementById('case-plus').addEventListener('click', function(){
     const caseNumber = caseInput.value;
     caseInput.value = parseInt(caseNumber) + 1;
 })
+document.getElementById('case-minus').addEventListener('click', function(){
+    // console.log('clicked')
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) - 1;
+})
