e24367bed6c54412b9e763740dad4ff98fd6a658
BabulKumar2022 <bb3583400@gmail.com>
2022-10-21 22:04:38 +0600

    total update

--- app.js
+++ app.js
@@ -7,10 +7,38 @@ function updateProductNumber(product, price, isIncreasing){
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
-    // update case total
+    // update product total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
+
+
 }
+
+//grant-total with tax
+function getInputValue(product){
+    const productInput = document.getElementById(product +'-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+const phoneTotal = getInputValue('phone') * 1229;
+const caseTotal = getInputValue('case') * 59;
+const subTotal = caseTotal + phoneTotal;
+const tax = subTotal/10;
+const totalPrice = subTotal + tax;
+// console.log(subTotal);
+document.getElementById('sub-total').innerText = subTotal;
+document.getElementById('tax-amount').innerText = tax;
+document.getElementById('total-price').innerText = totalPrice;
+
+}
+
+
+
+
+
 //phone handler
 document.getElementById('phone-plus').addEventListener('click', function(){
     updateProductNumber('phone', 1229, true)
